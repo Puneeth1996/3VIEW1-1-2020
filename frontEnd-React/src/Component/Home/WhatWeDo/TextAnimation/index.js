c58ec925aa7bc2b9c4083970d8e6feecc0b30fb0
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-06 17:00:26 +0530

    WOrking on javascript simple typing carousel

--- frontEnd-React/src/Component/Home/WhatWeDo/TextAnimation/index.js
+++ frontEnd-React/src/Component/Home/WhatWeDo/TextAnimation/index.js
@@ -3,11 +3,26 @@ import React, { Component } from 'react'
 import style from './index.module.css'
 
 export default class TextAnimation extends Component {
+    
     render() {
+
+
+        var i = 0;
+        var txt = 'Lorem ipsum dummy text blabla.';
+        var speed = 50;
+
+
         return (
             <div className={style.TextAnimation}>
                 {/* https://codepen.io/puneeth1996p/pen/povprGm */}
                 {/* https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter */}
+                <span
+                    class="txt-rotate"
+                    data-period="2000"
+                    data-rotate='["Home that needs to visualised ", 
+                    "Home that are to be built", 
+                    "Home to be redesigned." ]'>
+                </span>
             </div>
         )
     }
