8da63799fbdf505c21eebaf0476f0ed3ccef33d9
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-06 14:22:24 +0530

    Working structure for changing the background image on URL change

--- frontEnd-React/src/Component/CommonComponent/Banner/index.js
+++ frontEnd-React/src/Component/CommonComponent/Banner/index.js
@@ -4,50 +4,43 @@ import style from './index.module.css'
 export default class Banner extends Component {
 
 
-
-
-    // if (this.props.match.params.number){
-    //   // bannercontainer background = url 1
-    // }
-    // else if (props.martch.params == "Listings"){
-    //   // bannercontainer background = url 1
-    // }
-    // else if (props.martch.params == "News") {
-    //   // bannercontainer background = url 1
-    // }
-    // else if (props.martch.params == "Contact"){
-    //   // bannercontainer background = url 1
-    // }
-    //
-    // else {
-    //   // shuffle some background image
-    // }
-
-
     render() {
 
+
         console.log(window.location.href.split("/"));
+
         if(window.location.href.split("/").includes("About")){
           console.log('we are in about page ');
+          var bg = require('./images/about.jpg');
         }
         else if (window.location.href.split("/").includes("Listings")) {
           console.log('we are in Listings page ');
+          var bg = require('./images/listings.jpg');
         }
         else if (window.location.href.split("/").includes("News")) {
           console.log('we are in News page ');
+          var bg = require('./images/news.jpg');
         }
         else if (window.location.href.split("/").includes("Contact")) {
           console.log('we are in Contact page ');
+          var bg = require('./images/contact.jpg');
         }
         else {
+          console.log('Other pages we shall suffles with collection of background images');
           // shuffle the background image
+          var myArray = [1,2,3,4];
+          var rand = myArray[Math.floor(Math.random() * myArray.length)];
+          console.log(rand);
+          var bg = require('./images/' + rand + '.jpg');
         }
+
+        const bannerDiv = {backgroundImage: "url("+bg+")",};
 
 
 
 
         return (
-            <div className={style.bannercontainer}>
+            <div className={style.bannercontainer} style={bannerDiv}>
                 <p className={style.bannertitle}>{this.props.children}</p>
             </div>
         )
