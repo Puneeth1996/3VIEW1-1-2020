da900c239e83ea98e905dfd65acb3c2786dff605
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-06 14:39:41 +0530

    Working banner component that changes the images based on the url change

--- frontEnd-React/src/Component/CommonComponent/Banner/index.js
+++ frontEnd-React/src/Component/CommonComponent/Banner/index.js
@@ -7,32 +7,46 @@ export default class Banner extends Component {
     render() {
 
 
-        console.log(window.location.href.split("/"));
+        console.log();
+        var browserLoc = window.location.href.split("/");
+        console.log(browserLoc[4]);
 
-        if(window.location.href.split("/").includes("About")){
-          console.log('we are in about page ');
+
+        var myArray = [1,2,3,4,5,6,7,8];
+        var rand = myArray[Math.floor(Math.random() * myArray.length)];
+        console.log(rand);
+
+        if(browserLoc[3] == 'About'){
+          // console.log('we are in about page ');
           var bg = require('./images/about.jpg');
         }
-        else if (window.location.href.split("/").includes("Listings")) {
-          console.log('we are in Listings page ');
-          var bg = require('./images/listings.jpg');
-        }
-        else if (window.location.href.split("/").includes("News")) {
-          console.log('we are in News page ');
-          var bg = require('./images/news.jpg');
-        }
-        else if (window.location.href.split("/").includes("Contact")) {
-          console.log('we are in Contact page ');
+
+        else if (browserLoc[3] == 'Contact') {
+          // console.log('we are in Contact page ');
           var bg = require('./images/contact.jpg');
         }
-        else {
-          console.log('Other pages we shall suffles with collection of background images');
+
+
+        else if(browserLoc[4] && browserLoc[4]){
+          // console.log('Other pages we shall suffles with collection of background images');
           // shuffle the background image
-          var myArray = [1,2,3,4];
-          var rand = myArray[Math.floor(Math.random() * myArray.length)];
-          console.log(rand);
           var bg = require('./images/' + rand + '.jpg');
         }
+
+        else if (browserLoc[3] == 'Listings') {
+          // console.log('we are in Listings page ');
+          var bg = require('./images/listings.jpg');
+        }
+        else if (browserLoc[3] == 'News') {
+          // console.log('we are in News page ');
+          var bg = require('./images/news.jpg');
+        }
+
+        else {
+          var bg = require('./images/' + rand + '.jpg');
+        }
+
+
 
         const bannerDiv = {backgroundImage: "url("+bg+")",};
 
