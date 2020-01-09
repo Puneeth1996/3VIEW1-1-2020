38657b43f4c4da3db2f21c4484a766fd3dd192c8
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-09 16:33:34 +0530

    settling all the code for singlePage  New

--- frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
+++ frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
@@ -8,7 +8,8 @@ export default class SingleNewPost extends Component {
         const { id,Blog_unique_id,Publish_date,Main_title,Sub_title,Author,category,desp_small,desp_full,img } = this.props.blog_data.Blog_data_single
         return (
             <div className={style.SingleNewPost}>
-                {img}
+
+                <img src={img} />
                 <br/><br/>
                 {Main_title}
                 <br/><br/>
