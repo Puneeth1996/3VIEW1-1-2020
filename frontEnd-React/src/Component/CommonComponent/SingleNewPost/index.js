8f24b2deea87253a98675b316c933d65adb95a9f
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-09 14:57:50 +0530

    Working Code For the Single Blog Page

--- frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
+++ frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
@@ -8,11 +8,16 @@ export default class SingleNewPost extends Component {
         const { id,Blog_unique_id,Publish_date,Main_title,Sub_title,Author,category,desp_small,desp_full,img } = this.props.blog_data.Blog_data_single
         return (
             <div className={style.SingleNewPost}>
-                SingleNewPost
-                <br/>
+                {img}
+                <br/><br/>
                 {Main_title}
-                <br/>
-                On {Publish_date}  By {Author} Category {category}
+                <br/><br/>
+                By {Author} On {Publish_date} Category {category}
+                <br/><br/><br/><br/>
+                {Sub_title}
+                <br/><br/><br/><br/><br/><br/><br/><br/>
+                {desp_full}
+
             </div>
         )
     }
