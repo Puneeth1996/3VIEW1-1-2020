9b041b1b46dcfb2169d6a3f5066f8bde3af72596
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-10 14:56:50 +0530

    Working with searchForm

--- frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
+++ frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react'
 import style from './index.module.css'
 
 export default class ListingsSearchForm extends Component {
+
+
+
+    listingSearchData = (event) => {
+        event.preventDefault()
+        console.log("submitting the listingSearchData")
+    }
+
+    changeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        console.log(name,value)
+    }
+
+
     render() {
         return (
             <div className={style.ListingsSearchForm}>
 
-                <form action="/action_page.php">
-                    First name:<br/>
-                    <input type="text" name="firstname" value="" />
+                <form action="#" method="post" className={style.listingsForm}>
+                    Property ID:<br/>
+                    <input type="text" name="propertyID" value="" onChange={this.changeHandler} />
                     <br/>
-                    Last name:<br/>
-                    <input type="text" name="lastname" value="" />
+                    Property Name:<br/>
+                    <input type="text" name="propertyName" value="" onChange={this.changeHandler} />
                     <br/><br/>
-                    <input type="submit" value="Submit" />
+                    <input type="submit" value="Search Your Home" onClick={this.listingSearchData} />
                 </form> 
                 
             </div>
