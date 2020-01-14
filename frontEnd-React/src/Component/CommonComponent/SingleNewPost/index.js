1eec15a81796efaa5b4c7cf7cd7f846fbb732c07
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-14 15:37:23 +0530

    setting the component for singleNewPost

--- frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
+++ frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
@@ -8,17 +8,29 @@ export default class SingleNewPost extends Component {
         const { id,Blog_unique_id,Publish_date,Main_title,Sub_title,Author,category,desp_small,desp_full,img } = this.props.blog_data.Blog_data_single
         return (
             <div className={style.SingleNewPost}>
+                <div className={style.parallax}>
+                    {Main_title}
+                
+                    <div className={style.newMeta}>
+                        <div className={style.author}>
+                            <div className={style.author_icon}>
+                                <img src={require('./profile.svg')} alt=""/>
+                                <span>By {Author}</span>
+                            </div>
+                        </div>
+                        <div className={style.category}>
+                            <div className={style.category_icon}>
+                                <img src={require('./favorite.svg')} alt=""/>
+                                <span>Category {category}</span>
+                            </div>
+                        </div>
+                        <div className={style.postDate}>
+                            <img src={require('./calendar.png')} alt=""/>
+                            <span>On {Publish_date}</span>
+                        </div>
+                    </div>
 
-                <img src={process.env.PUBLIC_URL + '/News/news_1.jpg'} />
-                <br/><br/>
-                {Main_title}
-                <br/><br/>
-                By {Author} On {Publish_date} Category {category}
-                <br/><br/><br/><br/>
-                {Sub_title}
-                <br/><br/><br/><br/><br/><br/><br/><br/>
-                {desp_full}
-
+                </div>
             </div>
         )
     }
