47d1883e612f04bdc0ab988b73489b716c47f3dc
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-14 17:16:55 +0530

    Working responsive code for the sigleNewPost component

--- frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
+++ frontEnd-React/src/Component/CommonComponent/SingleNewPost/index.js
@@ -9,25 +9,31 @@ export default class SingleNewPost extends Component {
         return (
             <div className={style.SingleNewPost}>
                 <div className={style.parallax} style={{backgroundImage: "url(" + process.env.PUBLIC_URL + "/News/news_1.jpg)"}}>
-                    <div className={style.newsTitle}>{Main_title}</div>             
-                    <div className={style.newMeta}>
-                        <div className={style.author}>
-                            <div className={style.author_icon}>
-                                <img src={require('./profile.svg')} alt=""/>
-                                <span>By {Author}</span>
+                    <div className={style.parallaxContent}>
+                        <div className={style.newsTitle}>{Main_title}</div>             
+                        <div className={style.newMeta}>
+                            <div className={style.author}>
+                                <div className={style.author_icon}>
+                                    <img src={require('./profile.svg')} alt=""/>
+                                    <span>By {Author}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={style.category}>
-                            <div className={style.category_icon}>
-                                <img src={require('./favorite.svg')} alt=""/>
-                                <span>Category {category}</span>
+                            <div className={style.category}>
+                                <div className={style.category_icon}>
+                                    <img src={require('./favorite.svg')} alt=""/>
+                                    <span>Category {category}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={style.postDate}>
-                            <img src={require('./calendar.png')} alt=""/>
-                            <span>On {Publish_date}</span>
+                            <div className={style.postDate}>
+                                <img src={require('./calendar.png')} alt=""/>
+                                <span>On {Publish_date}</span>
+                            </div>
                         </div>
                     </div>
+                    
+                </div>
+                <div className={style.contentDesc}>
+                    <p>{desp_full}</p>
                 </div>
             </div>
         )
