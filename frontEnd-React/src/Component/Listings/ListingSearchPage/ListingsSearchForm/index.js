a83ec38c1c205cc9095bd56e05386d37b119962a
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-16 16:28:27 +0530

    starting to make the search field input in listings page

--- frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
+++ frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
@@ -7,24 +7,12 @@ export default class ListingsSearchForm extends Component {
         const { propertyID,dataVerificationMessage } = this.props.searchData
         return (
             <div className={style.ListingsSearchForm}>
-
                 <form action="#" method="post" className={style.listingsForm}>
-
-                    {/* <div className={style.search_box_title}>
-                        <div className={style.search_box_title_inner}>  
-                            <div className={style.search_box_title_icon}><img src={process.env.PUBLIC_URL + '/listing/search.png'} onClick={this.props.listingSearchData}/></div>
-                            <input type="text" name="propertyID" value={propertyID} onChange={this.props.changeHandler} />
-                        </div>
-                    </div> */}
-                    {/* Property ID:<br/>
-                    <input type="text" name="propertyID" value={propertyID} onChange={this.props.changeHandler} />
-                    <input type="submit" value="Search Your Home" onClick={this.props.listingSearchData} />
-                    <p>{dataVerificationMessage}</p> */}
-
                     <div className={style.search}>
-                        <input type="text" className={style.searchTerm} placeholder="What are you looking for?"/>
-                        <div className={style.search_box_title_icon}><img src={process.env.PUBLIC_URL + '/listing/search.png'} onClick={this.props.listingSearchData}/></div>
+                        <input className={style.searchTerm} type="text" name="propertyID" value={propertyID} onChange={this.props.changeHandler}/>
+                        <div className={style.search_box_title_icon}><img src={process.env.PUBLIC_URL + '/listing/search.png'} onClick={this.props.listingSearchData} /></div>
                     </div>
+                    <span className={style.searchDescription}>{dataVerificationMessage}</span>
                 </form> 
                 
             </div>
