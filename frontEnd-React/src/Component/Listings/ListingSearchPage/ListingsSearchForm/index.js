905c86bd97b4de0e038d0f44593ffb27b01624b2
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-16 17:07:10 +0530

    commiting of the working code for the day

--- frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
+++ frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
@@ -4,12 +4,18 @@ import style from './index.module.css'
 export default class ListingsSearchForm extends Component {
 
     render() {
+        const divStyle = {
+            color: 'blue',
+            boxSizing: 'unset',
+            margin: 'unset',
+            border: '3px solid #fd784f',
+        };
         const { propertyID,dataVerificationMessage } = this.props.searchData
         return (
             <div className={style.ListingsSearchForm}>
                 <form action="#" method="post" className={style.listingsForm}>
                     <div className={style.search}>
-                        <input className={style.searchTerm} type="text" name="propertyID" value={propertyID} onChange={this.props.changeHandler}/>
+                        <input style={divStyle} className={style.searchTerm} type="text" name="propertyID" value={propertyID} onChange={this.props.changeHandler}/>
                         <div className={style.search_box_title_icon}><img src={process.env.PUBLIC_URL + '/listing/search.png'} onClick={this.props.listingSearchData} /></div>
                     </div>
                     <span className={style.searchDescription}>{dataVerificationMessage}</span>
