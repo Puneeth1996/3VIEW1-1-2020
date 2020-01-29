7343630049a7b4fa13b2e7655583c09a1acf37e3
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-29 15:52:40 +0530

    Working code can add duplicates for the additon of persons

--- frontEnd-React/src/Component/CommonComponent/PersonCard/index.js
+++ frontEnd-React/src/Component/CommonComponent/PersonCard/index.js
@@ -4,6 +4,10 @@ import style from './index.module.css'
 export default class PersonCard extends Component {
     render() {
         return (
+        <div className={style.PersonCardRow}>
+
+
+
             <div className={style.PersonCard}>
                 <div className={style.card}>
                     <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" className={style.personImg} />
@@ -16,9 +20,49 @@ export default class PersonCard extends Component {
                         <a className={style.linkTag} href="#"><i className="fa fa-linkedin"></i></a>  
                         <a className={style.linkTag} href="#"><i className="fa fa-facebook"></i></a> 
                     </div>
-                    <p><button className={style.contactBtn} >Contact</button></p>
                 </div>
             </div>
+
+
+
+
+            <div className={style.PersonCard}>
+                <div className={style.card}>
+                    <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" className={style.personImg} />
+                    <h1>John Doe</h1>
+                    <p className={style.title}>CEO & Founder, Example</p>
+                    <p>Harvard University</p>
+                    <div className={style.contactLink}>
+                        <a className={style.linkTag} href="#"><i className="fa fa-dribbble"></i></a> 
+                        <a className={style.linkTag} href="#"><i className="fa fa-twitter"></i></a>  
+                        <a className={style.linkTag} href="#"><i className="fa fa-linkedin"></i></a>  
+                        <a className={style.linkTag} href="#"><i className="fa fa-facebook"></i></a> 
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div className={style.PersonCard}>
+                <div className={style.card}>
+                    <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" className={style.personImg} />
+                    <h1>John Doe</h1>
+                    <p className={style.title}>CEO & Founder, Example</p>
+                    <p>Harvard University</p>
+                    <div className={style.contactLink}>
+                        <a className={style.linkTag} href="#"><i className="fa fa-dribbble"></i></a> 
+                        <a className={style.linkTag} href="#"><i className="fa fa-twitter"></i></a>  
+                        <a className={style.linkTag} href="#"><i className="fa fa-linkedin"></i></a>  
+                        <a className={style.linkTag} href="#"><i className="fa fa-facebook"></i></a> 
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div> 
         )
     }
 }
