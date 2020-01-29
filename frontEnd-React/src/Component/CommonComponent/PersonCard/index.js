bb09b08273c28010b832c050f721a156c7f8acf8
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-29 15:38:02 +0530

    Working one contactCard

--- frontEnd-React/src/Component/CommonComponent/PersonCard/index.js
+++ frontEnd-React/src/Component/CommonComponent/PersonCard/index.js
@@ -5,7 +5,19 @@ export default class PersonCard extends Component {
     render() {
         return (
             <div className={style.PersonCard}>
-                PersonCard
+                <div className={style.card}>
+                    <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" className={style.personImg} />
+                    <h1>John Doe</h1>
+                    <p className={style.title}>CEO & Founder, Example</p>
+                    <p>Harvard University</p>
+                    <div className={style.contactLink}>
+                        <a className={style.linkTag} href="#"><i className="fa fa-dribbble"></i></a> 
+                        <a className={style.linkTag} href="#"><i className="fa fa-twitter"></i></a>  
+                        <a className={style.linkTag} href="#"><i className="fa fa-linkedin"></i></a>  
+                        <a className={style.linkTag} href="#"><i className="fa fa-facebook"></i></a> 
+                    </div>
+                    <p><button className={style.contactBtn} >Contact</button></p>
+                </div>
             </div>
         )
     }
