a87d6fd5c63882efd7c275889000cd20c38b723d
Puneeth1996 <puneeth1996p@gmail.com>
2020-01-29 14:05:17 +0530

    working from office

--- frontEnd-React/src/Component/Listings/ListingSearchPage/index.js
+++ frontEnd-React/src/Component/Listings/ListingSearchPage/index.js
@@ -32,7 +32,7 @@ export default class ListingSearchPage extends Component {
         event.preventDefault()        
         const form = new FormData()
         form.set('property_id', this.state.propertyID)
-        const response = await axios.post('http://localhost/3VIEW1-1-2020/REST-3V/listing/singleListing.php', form, {
+        const response = await axios.post('http://localhost:8080/3VIEW1-1-2020/REST-3V/listing/singleListing.php', form, {
             headers: { 'Content-Type': 'multipart/form-data' },
         })
         // console.log(response.data)
