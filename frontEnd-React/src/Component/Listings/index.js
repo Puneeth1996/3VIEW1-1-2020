08ee05f0a60ca0c7811c5f0d18224644dceecfcd
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-02 14:27:09 +0530

    Removing all the unwanted code for the project repo

--- frontEnd-React/src/Component/Listings/index.js
+++ frontEnd-React/src/Component/Listings/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import ListingsPage from './ListingsPage'
 import SpecificListing from './SpecificListing'
 
 import ListingSearchPage from './ListingSearchPage'
@@ -8,11 +7,6 @@ import ListingSearchPage from './ListingSearchPage'
 export default class Listings extends Component {
     render() {
         return (
-            // <Switch>
-            //     <Route exact path='/Listings' component={ListingsPage}/>
-            //     <Route path='/Listings/:number' component={SpecificListing}/>
-            // </Switch>
-
             <Switch>
                 <Route exact path='/Listings' component={ListingSearchPage}/>
                 <Route path='/Listings/:number' component={SpecificListing}/>
