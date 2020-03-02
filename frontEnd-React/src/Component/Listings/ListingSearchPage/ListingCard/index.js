c17a23372da5dde341b68dce32f00dd441c9adba
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-02 14:15:06 +0530

    this makes up for the validation of 6digit pin

--- frontEnd-React/src/Component/Listings/ListingSearchPage/ListingCard/index.js
+++ frontEnd-React/src/Component/Listings/ListingSearchPage/ListingCard/index.js
@@ -12,18 +12,20 @@ class ListingCard extends Component {
 
     constructor(props) {
         super(props)
-
         this.state = {
-            enteredPIN: '',
             reqPIN: '',
+            refire: false,
         }
     }
 
     checkValidity = (eve) => {
         console.log(eve)
-        this.setState({
-            enteredPIN: eve,
-        })       
+        if(eve==this.state.reqPIN){
+            console.log("its a match")
+            this.setState({
+                refire: true,
+            })
+        }
     }
 
     componentDidMount(){
@@ -36,29 +38,24 @@ class ListingCard extends Component {
     render() {
         const { property_id, house_name, property_img_loc, property_visuals_type, date_created, geospacial_data, area, price, property_features, property_desc, property_desc_full, addresses, property_id_ref_num, sixDigitPIN, mtl_file_loc, obj_file_loc, threeJS_iframe_url, react360_iframe_url, property_sale_availablity, property_legal_desc } = this.props.listingData.singleListingData
         var arrFeatures = property_features.split("|")
-        console.log(sixDigitPIN,this.state.reqPIN)
-        console.log(typeof this.state.enteredPIN,typeof this.state.reqPIN)
-        console.log(this.state.enteredPIN==this.state.reqPIN)
         return (
-
             <div className={style.Listingcard}>
                 <h2>{house_name}</h2>
                 <div className={style.card}>
                     <img src={property_img_loc} alt="Avatar" className={style.cardImage}/>
                     <div className={style.container}>
                         <h4><b>{property_visuals_type}</b></h4>
-                        <br/>
-                        <p>{property_desc}</p>
-                        <br/>
+                        <br/><p>{property_desc}</p><br/>
                         <p>{addresses}</p>
                     </div>
-                    <TagFeature tag={arrFeatures} className={style.TagFeature}/>
-                    { 
-                        this.state.enteredPIN==this.state.reqPIN ?
-                        <Redirect to='/About' /> :
-                        <ReactCodeInput type='password' fields={6} inputStyleInvalid={{color:'red'}} onChange={this.checkValidity} /> 
+                    {
+                        this.state.refire?
+                        <Redirect to='/About' />:
+                        <ReactCodeInput type='password' fields={6} onChange={this.checkValidity} />
+
                     }
                     
+                    <TagFeature tag={arrFeatures} className={style.TagFeature}/>
                 </div>
             </div>
         );
