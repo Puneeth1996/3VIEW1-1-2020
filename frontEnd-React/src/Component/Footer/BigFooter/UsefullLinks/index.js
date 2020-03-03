35532f390e1c09350b023bd285689f1f170a4f1b
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-03 11:10:11 +0530

    1st set of warning cleared moving to next set

--- frontEnd-React/src/Component/Footer/BigFooter/UsefullLinks/index.js
+++ frontEnd-React/src/Component/Footer/BigFooter/UsefullLinks/index.js
@@ -7,14 +7,13 @@ export default class UsefullLinks extends Component {
             <div className={`${style.usefulllinks}`}>
                 <div className={style.footercoltitle}>Usefull Links</div>
                 <ul className={style.footerusefullinks}>
-                    <li className={style.usefullinksitem}><a href="#">Listings</a></li>
-                    <li className={style.usefullinksitem}><a href="#">Favorite Cities</a></li>
-                    <li className={style.usefullinksitem}><a href="#">Clients Testimonials</a></li>
-                    <li className={style.usefullinksitem}><a href="#">Featured Listings</a></li>
-                    <li className={style.usefullinksitem}><a href="#">Properties on Offer</a></li>
-                    <li className={style.usefullinksitem}><a href="#">Services</a></li>
-                    <li className={style.usefullinksitem}><a href="#">News</a></li>
-                    <li className={style.usefullinksitem}><a href="#">Our Agents</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">Listings</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">Favorite Cities</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">Clients Testimonials</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">Featured Listings</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">Properties on Offer</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">Services</a></li>
+                    <li className={style.usefullinksitem}><a href="/#">News</a></li>
                 </ul>
             </div>
         )
