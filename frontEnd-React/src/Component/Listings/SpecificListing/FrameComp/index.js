bc8acf685ec235e6fa53f2ba8d06ee38586fa8b7
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-04 10:58:07 +0530

    introducing the react-iframe package

---
+++ frontEnd-React/src/Component/Listings/SpecificListing/FrameComp/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FrameComp extends Component {
+
+    render() {
+        return (
+            <h1>FrameComp</h1>
+        );
+    }
+}
+
+export default FrameComp;
