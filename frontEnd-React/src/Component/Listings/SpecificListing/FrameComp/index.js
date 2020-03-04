440907a1d79624f4e640b60cfb2cda29811e5cf4
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-04 12:12:46 +0530

    this is feeling good

--- frontEnd-React/src/Component/Listings/SpecificListing/FrameComp/index.js
+++ frontEnd-React/src/Component/Listings/SpecificListing/FrameComp/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Iframe from 'react-iframe'
+import style from './index.module.css'
 
 class FrameComp extends Component {
     render() {
         return (
-            <Iframe url="http://www.youtube.com/embed/2xmmxur_3KQ"
-                    width="450px"
-                    height="450px"
-                    id="myId"
-                    className="myClassname"
+            <Iframe url="http://thepuneeth1996p.tech/new_project/Creating%20the%20Mouse%20Control/"
+                    id="frameViewID"
+                    className={style.frameView}
                     display="initial"
                     position="relative"
             />
