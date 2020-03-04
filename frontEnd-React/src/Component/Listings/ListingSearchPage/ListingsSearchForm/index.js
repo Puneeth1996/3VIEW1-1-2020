08f96d434c72f45882ce517b20a46a9c83c235b4
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-04 10:46:33 +0530

    working based on 6digit pin and prop-id-ref-num

--- frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
+++ frontEnd-React/src/Component/Listings/ListingSearchPage/ListingsSearchForm/index.js
@@ -12,12 +12,12 @@ export default class ListingsSearchForm extends Component {
             margin: 'unset',
             border: '3px solid #fd784f',
         };
-        const { propertyID,} = this.props.searchData
+        const { property_id_ref_num,} = this.props.searchData
         return (
             <div className={style.ListingsSearchForm}>
                 <form method="POST" onSubmit={this.props.listingSearchData} className={style.listingsForm}>
                     <div className={style.search}>
-                        <input style={divStyle} className={style.searchTerm} type="text" name="propertyID" value={propertyID} onChange={this.props.changeHandler} />
+                        <input style={divStyle} className={style.searchTerm} type="text" name="property_id_ref_num" value={property_id_ref_num} onChange={this.props.changeHandler} />
                         <div className={style.search_box_title_icon}><img src={search} onClick={this.props.listingSearchData} alt='' /></div>
                     </div>
                     {/* <span className={style.searchDescription}>{dataVerificationMessage}</span> */}
