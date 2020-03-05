5631cf44c2ffb3eb80cd55a3d19588c61539f846
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2020-03-05 12:26:23 +0530

    the current styling seems good

--- frontEnd-React/src/Component/Listings/SpecificListing/GoogleMapComp/index.js
+++ frontEnd-React/src/Component/Listings/SpecificListing/GoogleMapComp/index.js
@@ -10,18 +10,18 @@ class GoogleMapComp extends Component {
     render() {
         const mystyle = {
             height: '40vh',
-            width: '100%',
+            width: '60%',
             color: "white",
             backgroundColor: "#07122c",
-            margin: "0 5vw 0 0",
+            marginLeft: '20vw',
             fontFamily: "Arial"
         };
         const position = [51.505, -0.09]
         return (
             <Map center={position} zoom={13} style={mystyle}>
                 <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
                 <Marker position={position}>
                     <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
