7dae780619eab036b0d3be2986642d7f801bc564
Puneeth P <puneeth1996p@gmail.com>
2020-04-09 13:57:17 +0530

    starting to create the  inner view with react360

---
+++ model/example/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class example extends React.Component {
+  render() {
+    return (
+      <View >
+      
+      </View>
+    );
+  }
+};
+
+
+
+AppRegistry.registerComponent('example', () => example);
