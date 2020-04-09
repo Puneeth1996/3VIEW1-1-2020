e65e7604fd2afc4ebbc471b85a2a6582c341b724
Puneeth P <puneeth1996p@gmail.com>
2020-04-09 15:08:32 +0530

    working on it, importing the modeel into the scene

--- model/example/index.js
+++ model/example/index.js
@@ -4,13 +4,27 @@ import {
   StyleSheet,
   Text,
   View,
+  asset
 } from 'react-360';
+import Entity from 'Entity';
 
 export default class example extends React.Component {
   render() {
     return (
       <View >
-      
+        <Entity
+          style={{
+            transform: [
+              {translate: [0, 0, 0]},
+              {scale: 1000},
+            ],
+          }}
+          source={{
+            obj: asset('Modern House_1_obj.obj'),
+            mtl: asset('Modern House_1_obj.mtl')
+          }}
+          lit={true}
+        />
       </View>
     );
   }
